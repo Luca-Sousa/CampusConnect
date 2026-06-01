afa130e19f56220bcaad35590a9f5b1e1f84dc3b
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2026-06-01 19:39:48 -0300

    refactor: update database schema import and structure in drizzle client

--- Backend/src/drizzle/client.ts
+++ Backend/src/drizzle/client.ts
@@ -1,8 +1,9 @@
 import { neon } from "@neondatabase/serverless";
 import { env } from "../env";
 import { drizzle } from "drizzle-orm/neon-http";
-import * as schema from "./schema/auth";
+import * as authSchema from "./schema/auth";
+import * as postsSchema from "./schema/posts";
 
 const sql = neon(env.POSTGRES_URL);
 
-export const db = drizzle(sql, { schema });
+export const db = drizzle(sql, { schema: { ...authSchema, ...postsSchema } });
